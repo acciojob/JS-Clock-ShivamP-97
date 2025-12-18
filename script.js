fa5f372df495a696da0a54296753bbc9aedd74e1
ShivamP-97 <shivampathak19972@gmail.com>
2025-12-18 14:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-function setDate() {
-  const now = new Date();
+function setClock() {
+    const now = new Date();
 
-  const seconds = now.getSeconds();
-  const secondDegrees = seconds * 6; 
-  document.querySelector('.second-hand').style.transform = `rotate(${secondDegrees}deg)`;
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hour = now.getHours();
 
-  const mins = now.getMinutes();
-  const minsDegrees = mins * 6; 
-  document.querySelector('.min-hand').style.transform = `rotate(${minsDegrees}deg)`;
+    const secondsDegree = seconds * 6 + 90;
+    const minsDegree = mins * 6 + 90;
+    const hourDegree = (hour % 12) * 30 + mins * 0.5 + 90;
 
-  const hour = now.getHours();
-  const hourDegrees = (30 * hour) + (mins / 2);
-  document.querySelector('.hour-hand').style.transform = `rotate(${hourDegrees}deg)`;
+    function degreeToMatrix(deg) {
+        const rad = deg * Math.PI / 180;
+        const a = Math.cos(rad).toFixed(6);
+        const b = Math.sin(rad).toFixed(6);
+        const c = (-Math.sin(rad)).toFixed(6);
+        const d = Math.cos(rad).toFixed(6);
+
+        return `matrix(${a}, ${b}, ${c}, ${d}, 0, 0)`;
+    }
+
+    document.querySelector('.second-hand').style.transform = degreeToMatrix(secondsDegree);
+    document.querySelector('.min-hand').style.transform = degreeToMatrix(minsDegree);
+    document.querySelector('.hour-hand').style.transform = degreeToMatrix(hourDegree);
 }
 
-setInterval(setDate, 1000);
-setDate();
+setInterval(setClock, 1000);
+setClock();
